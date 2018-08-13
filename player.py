72b5ccd5bbe587e3c71e8bfb8046e0ec6c0d07ac
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-08-12 21:31:43 -0700

    Fixed format issues in player

--- player.py
+++ player.py
@@ -9,11 +9,12 @@ import json
 STATUS_URL = "http://dabney.caltech.edu:27036/status"
 
 dl_statuses = {}
-for filename in glob.glob("vids/*.mkv"):
-    dl_statuses[filename[5:-4]] = True
-    # if a .mkv exists already, it's a safe bet that it's downloaded
+for filename in glob.glob("vids/*"):
+    dl_statuses[filename[5:filename.find(".")]] = filename[filename.find(".")+1:]
+    # if a file exists already, it's a safe bet that it's downloaded
+print(dl_statuses)
 
-# id => True if saved, DownloadProgress tuple otherwise
+# id => file extension if saved, DownloadProgress tuple if download in progress
 DownloadProgress = namedtuple('DownloadProgress', 'start_time download_proc')
 DOWNLOAD_TIMEOUT = 69 # nice
 
@@ -27,7 +28,7 @@ player_proc = None
 def download_song(id):
     print("Starting download of {}".format(id))
 
-    proc = Popen(["youtube-dl", "https://youtube.com/watch?v=" + id, "-o", "vids/" + id ])
+    proc = Popen(["youtube-dl", "https://youtube.com/watch?v=" + id, "-o", "vids/" + id, "-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" ])
 
     dl_statuses[id] = DownloadProgress(time(), proc)
 def retry_download(id):
@@ -41,15 +42,17 @@ def retry_download(id):
 def check_downloads():
     for id in dl_statuses:
         status = dl_statuses[id]
-        if status is True:
+        if isinstance(status, str):
             continue
         status.download_proc.poll()
         return_code = status.download_proc.returncode
-        if return_code == 0:
+        downloaded_files = glob.glob("vids/{}.*".format(id))
+        if return_code == 0 and len(downloaded_files) > 0:
             print("Completed download of {}".format(id))
-            dl_statuses[id] = True # successfully downloaded
+            dl_statuses[id] = downloaded_files[0][downloaded_files[0].find(".")+1:] # successfully downloaded, get format
         elif (return_code is None and time() - status.start_time > DOWNLOAD_TIMEOUT) or (isinstance(return_code, int) and return_code > 0):
             retry_download(id)
+            # TODO better recovery from issues here: don't just keep trying to play a bad format, etc.
             # taking too long or download failed (returned nonzero without being killed)
 set_interval(check_downloads, 5)
 
@@ -60,24 +63,26 @@ def dl_queue():
     if data.get("current") is not None:
         to_download.add(data["current"])
     for id in to_download:
-        if not dl_statuses.get(id): # if status is true or DownloadProgress tuple, no action required
+        if not dl_statuses.get(id): # if status is a format or DownloadProgress tuple, no action required
             download_song(id)
 set_interval(dl_queue, 10)
 
 def play(id, start_time=0):
     print("play requested for {}, starting at {}, {}".format(id, start_time, dl_statuses.get(id)))
-    while dl_statuses.get(id) is not True:
+    while not isinstance(dl_statuses.get(id), str):
         sleep(1) # wait for song to download by the queue checker
 
     global current_song
     global current_song_start
     global player_proc
 
-    assert current_song is None
+    if current_song is not None:
+        return # another thread beat us to playing; avoid overlap
     current_song = id
+    vid_format = dl_statuses[id]
     current_song_start = time() - start_time
 
-    command = ["omxplayer", "vids/{}.mkv".format(id)]
+    command = ["omxplayer", "vids/{}.{}".format(id, vid_format)]
     # TODO: -o both
 
     if start_time != 0:
