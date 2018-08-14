e09dcdef5bcc5b5ace9f79b2aeb4649ac444a950
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-08-13 20:57:49 -0700

    fixed syntax issue

--- check_ip.php
+++ check_ip.php
@@ -2,7 +2,7 @@
 function valid_ip($ip) {
     $caltech_ips = '/131.215.[0-9]{1,3}.[0-9]{1,3}/';
 
-    if ($ip == '127.0.0.1' || ip == '::1')
+    if ($ip == '127.0.0.1' || $ip == '::1')
         return true;
     else if (preg_match($caltech_ips, $ip))
         return true;
