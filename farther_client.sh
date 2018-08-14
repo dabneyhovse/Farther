fa4aeeb98af31dfedb4d14f2532fb13e92d91caa
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-08-13 23:45:14 -0700

    add cleaner script

--- farther_client.sh
+++ farther_client.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
 cd /home/pi/farther-client
-rm -f vids/*.part
-# TODO: check if disk is almost full and run a "keep only queued" script
-
+python3.6 clean_vids.py > farther.log
 python3.6 client.py > farther.log 2>&1
