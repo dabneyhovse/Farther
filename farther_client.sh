b06293305ca69ca36deef32922dcd53e198426b3
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-08-13 23:18:02 -0700

    remove partial downloads on boot

--- farther_client.sh
+++ farther_client.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
 cd /home/pi/farther-client
-python3.6 client.py &> farther.log
+rm -f vids/*.part
+# TODO: check if disk is almost full and run a "keep only queued" script
+
+python3.6 client.py > farther.log 2>&1
