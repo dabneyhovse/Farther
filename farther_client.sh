34052e96d6394dfdc7598c926cb97ebcdba7489d
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-08-15 00:12:04 -0700

    log with >>

--- farther_client.sh
+++ farther_client.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 cd /home/pi/farther-client
-python3.6 clean_vids.py > farther.log
-python3.6 client.py > farther.log 2>&1
+python3.6 clean_vids.py >> farther.log
+python3.6 client.py >> farther.log 2>&1
